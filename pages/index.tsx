3b2df4d54758023a0255ee3db3cd8a89458a163d
osawa-koki <github@osawa.dev>
2023-02-26 23:54:23 +0900

    トップページの説明。

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,14 @@
-import Link from "next/link";
 import Layout from "../components/Layout";
-import pages from "../pages";
 import setting from "../setting";
 
 export default function Home() {
   return (
     <Layout menu={false} footer={false}>
       <div id='Index'>
-        <h1>Hello Next.js 💓💓💓</h1>
+        <h1>simple-base64💓</h1>
         <img id='Logo' src={`${setting.basePath}/tako.png`} alt="Logo" />
+        <p className="mt-5">BASE64エンコード・デコードするサイトです。</p>
         <div id="IndexLink">
-        {
-          pages.map((page, index: number) => {
-            return (
-              <Link key={index} href={page.path}>
-                {page.name}
-              </Link>
-            )
-          })
-        }
         </div>
       </div>
     </Layout>
