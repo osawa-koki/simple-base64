063bd5b0c7d051f8553f31f4c3dda5c565086d67
osawa-koki <github@osawa.dev>
2023-02-27 00:16:58 +0900

    🦖🦖🦖

--- setting.ts
+++ setting.ts
@@ -4,7 +4,7 @@ const Env = require('./next.config.js');
 const isProd = process.env.NODE_ENV === 'production';
 
 const Setting = {
-  title: '🐟 nextjs-template.modern 🐟',
+  title: '🦖 simple-base64 🦖',
   isProd,
   basePath: Env.basePath,
   apiPath: isProd ? '' : 'http://localhost:80',
