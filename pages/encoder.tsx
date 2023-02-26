c19ab65be9b5da0b27a0f6611eeb87e72283b88e
osawa-koki <github@osawa.dev>
2023-02-27 00:24:27 +0900

    エンコード処理書いた。

--- pages/encoder.tsx
+++ pages/encoder.tsx
@@ -5,6 +5,16 @@ import Layout from "../components/Layout";
 
 export default function EncoderPage() {
 
+  const [text, setText] = useState<string | null>(null);
+  const [result, setResult] = useState<string | null>(null);
+
+  const Encode = () => {
+    if (text === null) {
+      return;
+    }
+    const encoded = Buffer.from(text).toString('base64');
+    setResult(encoded);
+  };
 
   return (
     <Layout>
@@ -12,14 +22,30 @@ export default function EncoderPage() {
         <h1>BASE64 Encoder</h1>
         <p>データをBASE64形式に変換します。</p>
         <Form>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Group className="mb-3">
             <Form.Label>変換するデータ</Form.Label>
-            <Form.Control as="textarea" rows={7} />
+            <Form.Control as="textarea" rows={7} onInput={(e) => {setResult(null); setText((e.target as HTMLTextAreaElement).value)}} />
           </Form.Group>
-          <Button variant="primary">
+          <Button variant="primary" onClick={Encode}>
             変換
           </Button>
         </Form>
+        {
+          result !== null && (
+            <>
+              <hr />
+              <Form className="mt-3">
+                <Form.Group className="mb-3">
+                  <Form.Label>BASE64エンコードされたデータ</Form.Label>
+                  <Form.Control as="textarea" rows={7} value={result} readOnly />
+                </Form.Group>
+                <Button variant="success">
+                  コピー
+                </Button>
+              </Form>
+            </>
+          )
+        }
       </div>
     </Layout>
   );
